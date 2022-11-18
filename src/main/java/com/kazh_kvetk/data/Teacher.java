e8bfec65d914f4a81d72850a132fb16eb43592ca
Hexiez <sanpavkaz@gmail.com>
2022-11-18 12:46:04 +0400

    changed teacher entity

--- src/main/java/com/kazh_kvetk/data/Teacher.java
+++ src/main/java/com/kazh_kvetk/data/Teacher.java
@@ -24,8 +24,8 @@ public class Teacher {
   @Column(name = "degree")
   private String degree;
 
-  @Column(name = "title")
-  private String title;
+  @Column(name = "academicRank")
+  private String academicRank;
 
   @Column(name = "department")
   private String department;
@@ -45,12 +45,12 @@ public class Teacher {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     Teacher teacher = (Teacher) o;
-    return Objects.equals(fullName, teacher.fullName) && Objects.equals(degree, teacher.degree) && Objects.equals(title, teacher.title) && Objects.equals(department, teacher.department) && Objects.equals(phoneNumber, teacher.phoneNumber) && Objects.equals(email, teacher.email) && Objects.equals(themes, teacher.themes);
+    return Objects.equals(fullName, teacher.fullName) && Objects.equals(degree, teacher.degree) && Objects.equals(academicRank, teacher.academicRank) && Objects.equals(department, teacher.department) && Objects.equals(phoneNumber, teacher.phoneNumber) && Objects.equals(email, teacher.email) && Objects.equals(themes, teacher.themes);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(fullName, degree, title, department, phoneNumber, email, themes);
+    return Objects.hash(fullName, degree, academicRank, department, phoneNumber, email, themes);
   }
 
   public void addTheme(Theme theme) {
